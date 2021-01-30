744769a8dc54329317d926ae01f450d729440bf4
furfur-jiang <2282995214@qq.com>
2021-01-30 17:21:09 +0800

    完成穿梭框

--- README.md
+++ README.md
@@ -15,10 +15,11 @@ npm run serve
 npm run build
 ```
 
-### Lints and fixes files
-```
-npm run lint
-```
+穿梭框的实现思路：
+首先在temlate写左右两列，两个按钮
+通过改变左右两列的数据，vue自动响应式地改变对应内容，然后通过按钮触发即可。
+通过shuttle 函数实现过滤选中和未选择，分别赋予左右列，并合并原来的内容。
+注意，由于改变了原来的数据，所以要先处理好先后顺序，否则会由于原数据被改而后续操作出现问题。
 
 ### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+文章 [vue3+ts+ant-vue:手把手教你实现穿梭框简易版，配源码](https://blog.csdn.net/weixin_44523860/article/details/113441091).
--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,101 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="content">
+    <a-card style="width: 300px" title="左侧">
+      <ul>
+        <li v-for="item in leftData" :key="item.title">
+          <input type="checkbox" v-model="item.isCheck" />
+          <label>{{ item.title }}</label>
+        </li>
+      </ul>
+    </a-card>
+
+    <div class="btn-group">
+      <button @click="shuttle(0)">→</button>
+      <button @click="shuttle(1)">←</button>
+    </div>
+
+    <a-card style="width: 300px" title="右侧">
+      <ul>
+        <li v-for="item in rightData" :key="item.title">
+          <input type="checkbox" v-model="item.isCheck" />
+          <label>{{ item.title }}</label>
+        </li>
+      </ul>
+    </a-card>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-
+import { defineComponent, reactive, toRefs } from "vue";
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: {},
+  setup() {
+    const state = reactive({
+      leftData: [
+        {
+          title: "furfur-jiang-left",
+          isCheck: true,
+        },
+        {
+          title: "fur-left",
+          isCheck: true,
+        },
+        {
+          title: "furfur-left",
+          isCheck: false,
+        },
+        {
+          title: "furfurJiang-left",
+          isCheck: false,
+        },
+      ],
+      rightData: [
+        {
+          title: "furfur-jiang-right",
+          isCheck: true,
+        },
+      ],
+    });
+
+    const shuttle = (flag: number) => {
+      if (flag === 0) {
+        state.rightData = [
+          ...state.rightData,
+          ...state.leftData.filter((item) => item.isCheck),
+        ];
+        state.leftData = state.leftData.filter((item) => !item.isCheck);
+      }else{
+        state.leftData = [
+          ...state.leftData,
+          ...state.rightData.filter((item) => item.isCheck),
+        ];
+        state.rightData = state.rightData.filter((item) => !item.isCheck);
+      }
+    };
+    return {
+      ...toRefs(state),
+      shuttle,
+    };
+  },
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 15px;
+}
+.btn-group {
+  display: flex;
+  flex-direction: column;
+  margin: 15px;
+}
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
 }
 </style>
